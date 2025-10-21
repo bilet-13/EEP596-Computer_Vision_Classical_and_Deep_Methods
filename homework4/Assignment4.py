eaa1e2769939ccd4bc63262098d1aa4338a9e0b7
bilet-13 <r10922114@csie.ntu.edu.tw>
2025-10-21 14:31:02 -0700

    feat: Implement train_classfier for quttieon 2

--- homework4/Assignment4.py
+++ homework4/Assignment4.py
@@ -25,12 +25,12 @@ def CIFAR10_dataset_a():
     batch_size = 4
 
     trainset = torchvision.datasets.CIFAR10(root='./cifar10', train=True,
-                                            download=True, transform=transform)
+                                            download=False, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                               shuffle=True, num_workers=2)
 
     testset = torchvision.datasets.CIFAR10(root='./cifar10', train=False,
-                                           download=True, transform=transform)
+                                           download=False, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                              shuffle=False, num_workers=2)
 
@@ -38,33 +38,79 @@ def CIFAR10_dataset_a():
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
     images, labels = next(iter(trainloader))
-    print(images.shape)
+    imshow(torchvision.utils.make_grid(images), labels, classes)
     
     return images, labels
 
+def imshow(img, labels, classes):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    for i in range(len(labels)):
+        plt.text(i * 32 + 16, 0, classes[labels[i].item()],
+                 color='black', ha='center', va='bottom', fontsize=8, weight='bold')
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+# show images
 class Net(nn.Module):
-    # Use this function to define your network
-    # Creates the network
     def __init__(self):
         super().__init__()
-        # Inits the model layers
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        # Defines forward apth
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
 def train_classifier():
     # Creates dataset
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
+    batch_size = 4
+
+    trainset = torchvision.datasets.CIFAR10(root='./cifar10', train=True,
+                                            download=False, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                              shuffle=True, num_workers=2)
     # Creates Network 
     net = Net()
 
 
     # Defines loss function and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    for epoch in range(2):  # loop over the dataset for 2 iteration
-        pass
+    for epoch in range(2):  # loop over the dataset for 2 iterations
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
 
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
     # Saves the model weights after training
     PATH = './cifar_net_2epoch.pth'
     torch.save(net.state_dict(), PATH)
@@ -109,5 +155,5 @@ def hyperparameter_sweep():
     return None
 
 if __name__ == "__main__":
-    images, labels = CIFAR10_dataset_a()
-    # your text code here
+    train_classifier()
+    # images, labels = CIFAR10_dataset_a()
